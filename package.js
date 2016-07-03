1a77f55a56a6f92015f742893db93dc8af5b7f37
Rustam S <info@life2film.com>
2016-07-03 11:00:11 +0600

    Update package.js

--- package.js
+++ package.js
@@ -1,18 +1,22 @@
 Package.describe({
-  name: "lf-films"
+  name: "lifefilm:lf-films",
+  version: '0.0.1',
+  summary: 'video template for meteorjs telescope (nova react version)',
+  git: 'https://github.com/fortunto2/lf-films.git',
+  documentation: 'README.md'
 });
 
 Package.onUse( function(api) {
 
-  api.versionsFrom("METEOR@1.0");
+  api.versionsFrom("METEOR@1.3");
 
   api.use([
-    'fourseven:scss',
-    'nova:core',
-    'nova:base-components',
-    'nova:posts',
-    'jrudio:videojs',
-    'nova:users'
+    'fourseven:scss@3.4.1',
+    'nova:core@0.26.3-nova',
+    'nova:base-components@0.26.3-nova',
+    'nova:posts@0.26.3-nova',
+    'jrudio:videojs@5.4.4',
+    'nova:users@0.26.3-nova'
     ]);
 
   api.addFiles([
